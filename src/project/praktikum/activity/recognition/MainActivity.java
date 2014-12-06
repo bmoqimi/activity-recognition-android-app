238481a18af11b85271e0b2117bc527420febd4e
Alireza Khalili <aaa@aaa.com>
2014-12-06 06:28:04 +0100

    Maps V1,0 also there

--- src/project/praktikum/activity/recognition/MainActivity.java
+++ src/project/praktikum/activity/recognition/MainActivity.java
@@ -244,4 +244,9 @@ public class MainActivity extends Activity {
 		  Intent intent = new Intent(this, GPSActivity.class);
 		    startActivity(intent);
 	}
+	
+	public void openMap(View view) {
+		  Intent intent = new Intent(this, MapActivity.class);
+		    startActivity(intent);
+	}
 }
