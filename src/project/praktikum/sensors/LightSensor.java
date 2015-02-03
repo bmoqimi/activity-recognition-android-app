94e8a529036c7931f83a80ac65bbe7ddd14cfac8
Jalal Khademi <jalal.khademi66@gmail.com>
2015-02-03 01:12:05 +0100

    light and sound comparison is done

--- src/project/praktikum/sensors/LightSensor.java
+++ src/project/praktikum/sensors/LightSensor.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 
 /**
@@ -16,13 +17,14 @@ public class LightSensor implements SensorEventListener{
 
 	 private SensorManager mSensorManager;
 	 private Sensor mLight;
-	 int minLux = 0;
-	 public int currentLux = 0;
-	 int maxLux;
+	 private float currentLux = 0;
+	 private int counter = 0;
+	 private float Threshold;
 	 Context ctx;
 	
-	 public LightSensor(Context ctx) 
+	 public LightSensor(Context ctx , float Threshold) 
 	 {
+		 this.Threshold = Threshold;
 		 this.ctx = ctx;
 	     mSensorManager = (SensorManager)ctx.getSystemService(Context.SENSOR_SERVICE);
 	     mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
@@ -33,15 +35,23 @@ public class LightSensor implements SensorEventListener{
 	     mSensorManager.registerListener((SensorEventListener) this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
 	 }
 	 
-	 public void stopListening()
+	 public boolean stopListening()
 	 {
-	     //mSensorManager.unregisterListener((SensorListener) ctx);
+	     mSensorManager.unregisterListener((SensorListener) ctx);
+	     return currentLux > Threshold ? false : true;
 	 }
 	
+	 private void calcCurrentLux(float lux)
+	 {
+		 currentLux = currentLux * counter + lux;
+		 counter++;
+		 currentLux /= (float) counter;
+	 }
+	 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-		currentLux = (int) event.values[0];
+		calcCurrentLux(event.values[0]);
 	}
 
 	@Override
--- src/project/praktikum/sensors/Recorder.java
+++ src/project/praktikum/sensors/Recorder.java
@@ -1,14 +1,9 @@
 package project.praktikum.sensors;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
-import android.os.Environment;
+import android.util.Log;
 
 
 /**
@@ -26,55 +21,25 @@ import android.os.Environment;
 
 public class Recorder
 {
-
-	private static final int RECORDER_BPP = 16;
-	private static String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
-	private static String AUDIO_RECORDER_FOLDER = "AudioRecorder";
-	private static String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
-	private static int RECORDER_SAMPLERATE = 44100;
-	private static boolean writeDataIsFinished = false;
 	private static int[] mSampleRates = new int[] { 44100,22050,11025,8000};
 	 
 	private AudioRecord recorder = null;
 	private int bufferSize = 0;
 	private Thread recordingThread = null;
 	private boolean isRecording = false;
-	
-	
-	public Recorder()
-	{
+	private int threshold = 1000;
+	private int sumLoud = 0;
+	private int maxLoud;
+
+	public void setThreshold(int threshold) {
+		this.threshold = threshold;
+		sumLoud = 0;
 	}
-	     
-	private String getFilename()
+
+	public Recorder(int maxLoud)
 	{
-		String filepath = Environment.getExternalStorageDirectory().getPath();
-		File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-		 
-		if(!file.exists()){
-		        file.mkdirs();
-		}
-		 
-		return (file.getAbsolutePath() 
-				+ "/" 
-				+ System.currentTimeMillis() 
-				+ AUDIO_RECORDER_FILE_EXT_WAV);
-	}
-	     
-	private String getTempFilename()
-	{
-		String filepath = Environment.getExternalStorageDirectory().getPath();
-		File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-		 
-		if(!file.exists()){
-		        file.mkdirs();
-		}
-		 
-		File tempFile = new File(filepath,AUDIO_RECORDER_TEMP_FILE);
-		 
-		if(tempFile.exists())
-		        tempFile.delete();
-		 
-		return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
+		this.maxLoud = maxLoud;
+		//this.txt = txt;
 	}
 	
 	private AudioRecord findAudioRecord() {
@@ -117,79 +82,64 @@ public class Recorder
             recorder.startRecording();
          
         isRecording = true;
-         
+	
         recordingThread = new Thread(new Runnable() {
                  
                 @Override
                 public void run() {
-                        writeAudioDataToFile();
+                	analyzeRecordedBuffer();
                 }
         },"AudioRecorder Thread");
          
         recordingThread.start();
 	}
 	
-	private void writeAudioDataToFile()
+	private void analyzeRecordedBuffer()
 	{
-		writeDataIsFinished = false;
 		byte data[] = new byte[bufferSize];
-		String filename = getTempFilename();
-		FileOutputStream os = null;
-		 
-		try 
-		{
-			os = new FileOutputStream(filename);
-		} 
-		catch (FileNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 
+		
 		int read = 0;
-		 
-		if(null != os)
+		
+		while(isRecording)
 		{
-			while(isRecording)
-			{
-				read = recorder.read(data, 0, bufferSize);
+			read = recorder.read(data, 0, bufferSize);
 				 
-				if(AudioRecord.ERROR_INVALID_OPERATION != read)
-				{
-					try
-					{
-					        os.write(data);
-					} 
-					catch (IOException e) 
-					{
-					        e.printStackTrace();
-					}
-				}
-			}
-		         
-			try 
+			if(AudioRecord.ERROR_INVALID_OPERATION != read)
 			{
-				os.close();
-			} 
-			catch (IOException e) {
-		        e.printStackTrace();
-			}
-			finally
-			{
-				writeDataIsFinished = true;
+				int [] avg = RMS(data);
+				avg[0]++;
 			}
 		}
 	}
-	     
-	public void stopRecording()
+	
+	private int[] RMS(byte[] input)
 	{
-		int channel = 1;
+		int [] ans = new int[input.length/100 + 1];
+		for(int i = 0 ; i * 100 < input.length ; i++)
+		{
+			int j;
+			int sum = 0;
+			for(j = 0 ;i * 100 + j < input.length && j < 100 ; j++)
+			{
+				sum += (input[i * 100 + j] * input[i * 100 + j]);
+			}
+			ans[i] = (int) Math.sqrt((double)(sum/j));
+			if(ans[i] > threshold)
+			{
+				sumLoud++;
+				Log.i("sumLoud" , String.valueOf(sumLoud));
+			}
+		}
+		return ans;
+	}
+	
+	public boolean stopRecording()
+	{
 		if(null != recorder)
 		{
 			isRecording = false;
 			if(recorder.getState()==1)
 			{
-				channel = recorder.getChannelCount();
 			    recorder.stop();
 			}
 			recorder.release();
@@ -197,117 +147,6 @@ public class Recorder
 			recorder = null;
 			recordingThread = null;
 		}
-		 
-		copyWaveFile(getTempFilename(),getFilename(),channel);
-		deleteTempFile();
-	}
-
-	private void deleteTempFile()
-	{
-		File file = new File(getTempFilename());
-		 
-		file.delete();
-	}
-	     
-	private void copyWaveFile(String inFilename,String outFilename , int channel)
-	{
-		FileInputStream in = null;
-		FileOutputStream out = null;
-		long totalAudioLen = 0;
-		long totalDataLen = totalAudioLen + 36;
-		long longSampleRate = RECORDER_SAMPLERATE;
-		int channels = channel;
-		long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
-	         
-        byte[] data = new byte[bufferSize];
-	         
-		try 
-		{
-			in = new FileInputStream(inFilename);
-			out = new FileOutputStream(outFilename);
-			totalAudioLen = in.getChannel().size();
-			totalDataLen = totalAudioLen + 36;
-			 
-			//AppLog.logString("File size: " + totalDataLen);
-			 
-			WriteWaveFileHeader(out,
-					totalAudioLen,
-					totalDataLen,
-					longSampleRate,
-					channels,
-					byteRate);
-			 
-			while(in.read(data) != -1)
-			{
-				out.write(data);
-			}
-			 
-			in.close();
-			out.close();
-		} 
-		catch (FileNotFoundException e)
-		{
-	        e.printStackTrace();
-		} 
-		catch (IOException e) 
-		{
-	        e.printStackTrace();
-		}
-	}
-
-	private void WriteWaveFileHeader(
-	                FileOutputStream out, long totalAudioLen,
-	                long totalDataLen, long longSampleRate, int channels,
-	                long byteRate) throws IOException
-    {
-	         
-		byte[] header = new byte[44];
-		 
-		header[0] = 'R';  // RIFF/WAVE header
-		header[1] = 'I';
-		header[2] = 'F';
-		header[3] = 'F';
-		header[4] = (byte) (totalDataLen & 0xff);
-		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-		header[8] = 'W';
-		header[9] = 'A';
-		header[10] = 'V';
-		header[11] = 'E';
-		header[12] = 'f';  // 'fmt ' chunk
-		header[13] = 'm';
-		header[14] = 't';
-		header[15] = ' ';
-		header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-		header[17] = 0;
-		header[18] = 0;
-		header[19] = 0;
-		header[20] = 1;  // format = 1
-		header[21] = 0;
-		header[22] = (byte) channels;
-		header[23] = 0;
-		header[24] = (byte) (longSampleRate & 0xff);
-		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-		header[28] = (byte) (byteRate & 0xff);
-		header[29] = (byte) ((byteRate >> 8) & 0xff);
-		header[30] = (byte) ((byteRate >> 16) & 0xff);
-		header[31] = (byte) ((byteRate >> 24) & 0xff);
-		header[32] = (byte) (2 * 16 / 8);  // block align
-		header[33] = 0;
-		header[34] = RECORDER_BPP;  // bits per sample
-		header[35] = 0;
-		header[36] = 'd';
-		header[37] = 'a';
-		header[38] = 't';
-		header[39] = 'a';
-        header[40] = (byte) (totalAudioLen & 0xff);
-        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-        out.write(header, 0, 44);
+		return maxLoud < sumLoud ? false : true;
 	}
 }
