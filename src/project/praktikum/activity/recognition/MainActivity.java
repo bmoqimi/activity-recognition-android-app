84dd16b0a7dc71f260dbbdb6612c1fc571458869
Jalal Khademi <jalal.khademi66@gmail.com>
2015-03-04 22:08:32 +0100

    Main UI is improved

--- src/project/praktikum/activity/recognition/MainActivity.java
+++ src/project/praktikum/activity/recognition/MainActivity.java
@@ -5,8 +5,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import project.praktikum.activity.recognition.ActivityCaptureService;
 import project.praktikum.activity.report.ShowReport;
@@ -34,14 +37,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+	ExpandableListView xLv;
+	
 	DataBase db;
 	ListView lv;
 	Cursor c;
@@ -50,11 +53,11 @@ public class MainActivity extends ActionBarActivity {
 	boolean isServiceRunning;
 	boolean isFingerprintingServiceRunning;
 
-	Button btn;
-	Button btnFingerprinting;
+	//Button btn;
+	//Button btnFingerprinting;
 
-	Button btnExport;
-	ImageView imgCurrentActivity;
+	//Button btnExport;
+	//ImageView imgCurrentActivity;
 	Handler handler;
 
 	Messenger mFingerprintingService = null;
@@ -123,6 +126,7 @@ public class MainActivity extends ActionBarActivity {
 		}
 	};
 
+	@SuppressWarnings("static-access")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -134,33 +138,138 @@ public class MainActivity extends ActionBarActivity {
 			Toast.makeText(getApplicationContext(),"HOME IS NOT SET YET!", Toast.LENGTH_LONG).show();
 		
 		db = db.getInstance(getApplicationContext());
-		lv = (ListView) findViewById(R.id.lv);
+		//lv = (ListView) findViewById(R.id.lv);
 
 		c = db.getAllRecords();
 		
 		dbAdapter = new CustomCursorAdapter(getApplicationContext(), c, 0, db);
 
-		lv.setAdapter(dbAdapter);
+		//lv.setAdapter(dbAdapter);
+		xLv = (ExpandableListView) findViewById(R.id.expandableListView1);
+		
+		db.fillTimeLine();
+        
+        loadHosts(prepareListData()); 
 
 		isFingerprintingServiceRunning = isMyServiceRunning(FingerprintingService.class);
-		btnFingerprinting = (Button) findViewById(R.id.ButtonFingerprintingService);
+		//btnFingerprinting = (Button) findViewById(R.id.ButtonFingerprintingService);
 
 		isServiceRunning = isMyServiceRunning(ActivityCaptureService.class);
-		btn = (Button) findViewById(R.id.btnStartService);
+		//btn = (Button) findViewById(R.id.btnStartService);
 
-		btnExport = (Button) findViewById(R.id.btnExport);
-		imgCurrentActivity = (ImageView) findViewById(R.id.imgCurrentActivity);
+		//btnExport = (Button) findViewById(R.id.btnExport);
+		//imgCurrentActivity = (ImageView) findViewById(R.id.imgCurrentActivity);
 
 		if (isFingerprintingServiceRunning) {
-			btnFingerprinting.setText("Stop Fingerprinting");
+			//btnFingerprinting.setText("Stop Fingerprinting");
 			doFingerprintingBindService();
 		}
 
 		if (isServiceRunning) {
-			btn.setText("STOP CAPTURE");
+			//btn.setText("STOP CAPTURE");
 			doBindService();
 		}
 	}
+	
+    @SuppressWarnings("deprecation")
+	private ArrayList<Parent> prepareListData() {
+    	
+    	final ArrayList<Parent> list = new ArrayList<Parent>();
+    	
+        Date currentDay = new Date();
+        Date currentActivity = new Date();
+    	Cursor c = db.fetchTimeLine();
+    	if(c.moveToFirst())
+    	{
+    		do
+    		{
+    			Parent header = new Parent();
+    			currentActivity = stringToDate(c.getString(2));
+            	currentDay = currentActivity;
+            	header.setDate(currentDay);
+            	
+            	ArrayList<Child> children = new ArrayList<Child>();
+            	
+    			while(currentActivity.getDate() == currentDay.getDate() && !c.isAfterLast())
+    			{
+    				Child mChild = new Child();
+    				mChild.setActivity(c.getString(c.getColumnIndex("activity")));
+    				mChild.setStart(c.getString(c.getColumnIndex("start")));
+    				mChild.setEnd(c.getString(c.getColumnIndex("end")));
+    				mChild.setDuration(
+    						stringToDate(mChild.getEnd()).getTime() - 
+    						stringToDate(mChild.getStart()).getTime());
+    				children.add(mChild);
+    				setActivityTime(header,
+    						stringToDate(mChild.getEnd()).getTime() - stringToDate(mChild.getStart()).getTime(),
+    						mChild.getActivity());
+    				c.moveToNext();
+    				if(!c.isAfterLast())
+    					currentActivity = stringToDate(c.getString(2));
+    			}
+    			
+    			header.setChildren(children);
+    			list.add(header);
+    			
+    		}while(c.moveToNext());
+    	}
+    	return list;
+    }
+    
+    private void setActivityTime(Parent parent , long amount , String activity)
+    {
+    	if(activity.equals("Still"))
+    	{
+    		parent.setStill(parent.getStill() + amount);
+    	}
+    	
+    	else if(activity.equals("Walking"))
+    	{
+    		parent.setWalking(parent.getWalking() + amount);
+    	}
+    	else if(activity.equals("Sleeping"))
+    	{
+    		parent.setSleeping(parent.getSleeping() + amount);
+    	}
+    	else if(activity.equals("InVehicle"))
+    	{
+    		parent.setDriving(parent.getDriving() + amount);
+    	}
+    	else if(activity.equals("OnBicycle"))
+    	{
+    		parent.setCycling(parent.getCycling() + amount);
+    	}
+    	else if(activity.equals("Running"))
+    	{
+    		parent.setRunning(parent.getRunning() + amount);
+    	}
+    }
+    
+    private void loadHosts(final ArrayList<Parent> newParents)
+    {
+        if (newParents == null)
+            return;
+        if (xLv.getExpandableListAdapter() == null)
+        {
+            final ExpandableListAdapter mAdapter = new ExpandableListAdapter(this , newParents);
+            xLv.setAdapter(mAdapter);
+        }
+        else
+        {
+            xLv.deferNotifyDataSetChanged();
+        }   
+    }
+    
+    @SuppressLint("SimpleDateFormat")
+	private Date stringToDate(String aDate) {
+
+        if(aDate==null) return null;
+        ParsePosition pos = new ParsePosition(0);
+        SimpleDateFormat simpledateformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date stringDate = simpledateformat.parse(aDate, pos);
+        return stringDate;            
+
+     }
 
 	@SuppressWarnings("static-access")
 	private void updateLv() {
@@ -173,17 +282,17 @@ public class MainActivity extends ActionBarActivity {
 
 	private void setImgCurrentActivity(String avtivity) {
 		if (avtivity.equals("Unknown")) {
-			imgCurrentActivity.setImageResource(R.drawable.unknown);
+			//imgCurrentActivity.setImageResource(R.drawable.unknown);
 		} else if (avtivity.equals("In Vehicle")) {
-			imgCurrentActivity.setImageResource(R.drawable.in_vehicle);
+			//imgCurrentActivity.setImageResource(R.drawable.in_vehicle);
 		} else if (avtivity.equals("On Bicycle")) {
-			imgCurrentActivity.setImageResource(R.drawable.on_bike);
+			//imgCurrentActivity.setImageResource(R.drawable.on_bike);
 		} else if (avtivity.equals("On Foot")) {
-			imgCurrentActivity.setImageResource(R.drawable.on_foot);
+			//imgCurrentActivity.setImageResource(R.drawable.on_foot);
 		} else if (avtivity.equals("Still")) {
-			imgCurrentActivity.setImageResource(R.drawable.still);
+			//imgCurrentActivity.setImageResource(R.drawable.still);
 		} else {
-			imgCurrentActivity.setImageResource(R.drawable.tillting);
+			//imgCurrentActivity.setImageResource(R.drawable.tillting);
 		}
 	}
 
@@ -198,36 +307,36 @@ public class MainActivity extends ActionBarActivity {
 		return false;
 	}
 
-	public void startService(View view) {
+	public void startService() {
 		if (!isServiceRunning) {
 			Intent i = new Intent(getBaseContext(),
 					ActivityCaptureService.class);
 			startService(i);
 			isServiceRunning = true;
-			btn.setText("STOP CAPTURE");
+			//btn.setText("STOP CAPTURE");
 			doBindService();
 		} else {
 			doUnbindService();
 			stopService(new Intent(getBaseContext(),
 					ActivityCaptureService.class));
 			isServiceRunning = false;
-			btn.setText("START CAPTURE");
+			//btn.setText("START CAPTURE");
 		}
 	}
 
-	public void startFingerprintingService(View view) {
+	public void startFingerprintingService() {
 		if (!isFingerprintingServiceRunning) {
 			Intent i = new Intent(getBaseContext(), FingerprintingService.class);
 			startService(i);
 			isFingerprintingServiceRunning = true;
-			btnFingerprinting.setText("Stop Fingerprinting");
+			//btnFingerprinting.setText("Stop Fingerprinting");
 			doFingerprintingBindService();
 		} else {
 			doFingerprintingUnbindService();
 			stopService(new Intent(getBaseContext(),
 					FingerprintingService.class));
 			isFingerprintingServiceRunning = false;
-			btnFingerprinting.setText("Start Fingerprinting");
+			//btnFingerprinting.setText("Start Fingerprinting");
 		}
 	}
 
@@ -305,7 +414,7 @@ public class MainActivity extends ActionBarActivity {
 	}
 
 	@SuppressLint("SimpleDateFormat")
-	public void exportDB(View view) {
+	public void exportDB() {
 		File sd = Environment.getExternalStorageDirectory();
 		File data = Environment.getDataDirectory();
 		FileChannel source = null;
@@ -348,19 +457,12 @@ public class MainActivity extends ActionBarActivity {
 		startActivity(Intent.createChooser(sendIntent, "Send Attachment !:"));
 	}
 
-	// sendIntent.setType("image/jpeg");
-	// sendIntent.setType("message/rfc822");
-	// sendIntent.setType("*/*");
-	// sendIntent.putExtra(android.content.Intent.EXTRA_STREAM, uriToZip);
-	// startActivity(Intent.createChooser(sendIntent, "Send Attachment !:"));
-	// }
-
-	 public void SetHome_OnClick(View view) {
+	public void SetHome_OnClick() {
 	 Intent intent = new Intent(this, SetHome.class);
 	 startActivity(intent);
 	 }
 	 
-		public void HomeSimilarity_OnClick(View view) {
+	public void HomeSimilarity_OnClick() {
 			Intent intent = new Intent(this, HomeRecognition.class);
 
 			// intent.setType(LOCATION_SERVICE);
@@ -369,7 +471,7 @@ public class MainActivity extends ActionBarActivity {
 		}
 
 		@Override
-		public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 			if (requestCode == 1) {
 				if (resultCode == RESULT_OK) {
 					boolean isHome = data.getBooleanExtra("isHome", false);
@@ -378,7 +480,7 @@ public class MainActivity extends ActionBarActivity {
 			}
 		}
 	 
-	 public void ButtonReport_OnClick(View view) {
+	public void ButtonReport_OnClick() {
 	 Intent intent = new Intent(this, ShowReport.class);
 	 startActivity(intent);
 	 }
@@ -399,6 +501,31 @@ public class MainActivity extends ActionBarActivity {
 	             Intent intent = new Intent(getApplicationContext(), ActivityExpandableListView.class);
 	             startActivity(intent);
 	             return true;
+
+	         case R.id.action_report:
+	        	 ButtonReport_OnClick();
+	             return true;
+
+	         case R.id.action_export:
+	             exportDB();
+	             return true;
+
+	         case R.id.action_service:
+	        	 startService();
+	             return true;
+
+	         case R.id.action_fingertpring:
+	        	 startFingerprintingService();
+	             return true;
+
+	         case R.id.action_sethome:
+	        	 SetHome_OnClick();
+	             return true;
+
+	         case R.id.action_home_similarity:
+	        	 HomeSimilarity_OnClick();
+	             return true;
+	             
 	         default:
 	             return super.onOptionsItemSelected(item);
 	     }
